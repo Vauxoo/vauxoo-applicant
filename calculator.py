90b4aad7e3bb6d2fe296aea8c857a8926129f9d7
moylop260 <moylop260@vauxoo.com>
2015-05-04 18:21:43 -0500

    [REF] Add pylint test

--- calculator.py
+++ calculator.py
@@ -1,5 +1,15 @@
+"""
+Your module documentation here
+"""
+
 class CalculatorClass(object):
+    """
+    Your class documentation here
+    """
 
     def sum(self, num_list):
+        """
+        Your method documentation here
+        """
         # your sum code here
         return "not implement yet"  # Remove this dummy line
--- primes.py
+++ primes.py
@@ -1,5 +1,15 @@
+"""
+Your module documentation here
+"""
+
 class PrimeClass(object):
+    """
+    Your class documentation here
+    """
 
     def is_prime(self, num_int):
+        """
+        Your method documentation here
+        """
         # your primes code here
         return "not implement yet"  # Remove this dummy line
