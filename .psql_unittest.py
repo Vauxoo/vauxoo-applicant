970039283450c042436ee6dacab8b1403e86b610
moylop260 <moylop260@vauxoo.com>
2015-05-06 14:55:39 -0500

    [REF] add psql unittest

---
+++ .psql_unittest.py
@@ -0,0 +1,191 @@
+"""
+Script to test postgres scripts for applicant project
+"""
+
+import subprocess
+import unittest
+
+def run_output(cmd, cwd=None):
+    """
+    Method to run a command in OS
+    """
+    return subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, cwd=cwd).communicate()[0]
+
+
+def run_psql_query(query, dbname):
+    """
+    Method to run a psql query command in OS
+    """
+    cmd = ['psql', '-d', dbname, '-c', query]
+    res = run_output(cmd)
+    return res
+
+
+def psql_output2list(psql_output):
+    newline_count = 0
+    lines = []
+    for line in psql_output.split('\n'):
+        newline_count += 1
+        if newline_count == 2:
+            # don't append line of '---+---'
+            # returned by psql commands
+            continue
+        new_line = []
+        for item in line.split('|'):
+            item = item.strip()
+            new_line.append(item)
+        lines.append(new_line)
+    lines = lines[:-3]
+    return lines
+
+
+def psql_output2dict(psql_output):
+    psql_list = psql_output2list(psql_output)
+    return [dict(zip(psql_list[0], row)) for row in psql_list[1:]]
+
+
+class TestApplicantPostgres(unittest.TestCase):
+    """
+    Main class to test postgres scripts for applicant project
+    """
+
+    def setUp(self):
+        """
+        Method init of global unittest class
+        """
+        self.dbname = 'employee_employee'
+        self.required_tables = [
+            'employee',
+            'employee_department',
+            'employee_hobby',
+        ]
+        self.required_fields = {
+            'employee': [
+                'id', 'name', 'description',
+            ],
+            'employee_department': [
+                'id', 'name', 'description',
+            ],
+            'employee_hobby': [
+                'id', 'name', 'description',
+            ],
+        }
+        self.required_fields_qty = {
+            'employee': 5,
+            'employee_department': 3,
+            'employee_hobby': 3,
+        }
+        self.required_records = {
+            'employee': 4,
+            'employee_department': 6,
+            'employee_hobby': 3,
+        }
+
+    def test_05_db_tables_requested(self):
+        """
+        Method to verify that exists requested tables
+        """
+        print "Start test:" + \
+              self.test_05_db_tables_requested.__doc__
+        query = """SELECT table_name
+                FROM information_schema.tables
+                WHERE  table_schema='public'"""
+        res = run_psql_query(query, self.dbname)
+        res_dict = psql_output2dict(res)
+        table_names = [row['table_name'] for row in res_dict]
+        for required_table in self.required_tables:
+            self.assertEqual(
+                required_table in table_names, True,
+                "don't exists '%s' table requested" \
+                % (required_table))
+        if len(table_names) == 3:
+            print "WARNING: Additional table not implement yet"
+        self.assertEqual(
+            len(table_names) == 3 or len(table_names) == 4, True,
+            "You have created different quantity of tables expected.")
+        print "End test:" + \
+              self.test_05_db_tables_requested.__doc__
+
+    def test_10_db_fields_requested(self):
+        """
+        Method to get fields of tables 
+        and check that requested fields exists
+        """
+        print "Start test:" + \
+              self.test_10_db_fields_requested.__doc__
+        for table_name in self.required_fields:
+            query = """SELECT column_name
+                    FROM information_schema.columns
+                    WHERE table_name = '%s'"""%table_name
+            res = run_psql_query(query, self.dbname)
+            res_dict = psql_output2dict(res)
+            column_names = [row['column_name'] for row in res_dict]
+            if len(column_names) == 0:
+                print "WARNING: '%s' Not implement yet" % (table_name, )
+                return True
+            for required_field in self.required_fields[table_name]:
+                self.assertEqual(
+                    required_field in column_names, True,
+                    "don't exists '%s' column in table '%s'" \
+                    % (required_field, table_name))
+            self.assertEqual(
+                self.required_fields_qty[table_name], len(column_names),
+                "You have created different quantity of fields expected in table '%s'." \
+                % (table_name))
+
+        query = """SELECT table_name
+                FROM information_schema.tables
+                WHERE  table_schema='public'
+                AND table_name NOT IN (%s)""" % (
+                        "'" + "','".join(self.required_tables) + "'")
+        res = run_psql_query(query, self.dbname)
+        res_dict = psql_output2dict(res)
+        if len(res_dict) == 0:
+            print "WARNING: Relation not implement yet"
+            return True
+        self.assertEqual(
+            len(res_dict) in (0, 1), True,
+            "You have created different quantity of tables expected.")
+        new_table = res_dict[0]['table_name']
+        query = """SELECT column_name
+                    FROM information_schema.columns
+                    WHERE table_name = '%s'"""%new_table
+        res = run_psql_query(query, self.dbname)
+        res_dict = psql_output2dict(res)
+        column_names = [row['column_name'] for row in res_dict]
+        self.assertEqual(
+            len(column_names), 2,
+            "You have different quantity of columns" +
+            " expected in table '%s'."%new_table)
+        print "End test:" + \
+              self.test_10_db_fields_requested.__doc__
+
+    def test_20_db_records_requested(self):
+        """
+        Method to get numbers of records in tables 
+        and check that requested records is equal
+        to inserted records
+        """
+        print "Start test:" + \
+              self.test_20_db_records_requested.__doc__
+        for table_name in self.required_records:
+            query = """SELECT *
+                    FROM %s"""%table_name
+            res = run_psql_query(query, self.dbname)
+            res_dict = psql_output2dict(res)
+            if len(res_dict) == 0:
+                print "WARNING: '%s' Not implement yet" % (table_name, )
+                return True
+            self.assertEqual(
+                self.required_records[table_name], len(res_dict),
+                "Request records in table '%s'=%d. Records found=%d" \
+                % (table_name, self.required_records[table_name], len(res_dict)))
+        print "End test:" + \
+              self.test_20_db_records_requested.__doc__
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -3,13 +3,60 @@
 --       You can create database locally to test it.
 --       Consider add ';' at end sentence.
 
-CREATE TABLE employee (
-);
+--creacion de la tabla departmentos
+ create table employee_department(
+ id serial,
+ name varchar(20) not null,
+ description varchar(20),
+ primary key(id));
+ 
 
-CREATE TABLE employee_department (
-);
+---creamos la tabla employee_hobby
+create table employee_hobby(
+id serial,
+name varchar(20),
+description varchar(20),
+primary key(id));
 
-CREATE TABLE employee_hobby (
-);
+primary key(id));
 
--- ...
+
+ --creacion de la tabla empleados
+ create table employee(
+ id serial,
+ name varchar(20) not null,
+ description varchar(20),
+ --id_hobby smallint references employee_hobby(id) not null,
+ id_department smallint references employee_department(id) not null,
+ name_jefe varchar(10) unique not null,
+ primary key(id));
+
+
+create table otra(
+    id1 serial,
+    id2 serial
+ );
+
+ --insertar departmentos
+ insert into employee_department(name) values ('compras');
+ insert into employee_department(name) values ('ventas');
+ insert into employee_department(name) values ('contabilidad');
+ insert into employee_department(name) values ('produccion');
+ insert into employee_department(name) values ('desarrollo');
+ insert into employee_department(name) values ('sqa');
+
+ 
+ --insertar los hobbys
+insert into employee_hobby(name) values('jugar');
+insert into employee_hobby(name) values('leer');
+insert into employee_hobby(name) values('caminar');
+
+
+--insertar empleados
+ insert into employee(name,id_department,name_jefe) values('jose',1,'javier');
+ insert into employee(name,id_department,name_jefe) values('jesus',2,'mendoza');
+ insert into employee(name,id_department,name_jefe) values('juan',3,'alexis');
+ insert into employee(name,id_department,name_jefe) values('janh',3,'pablo');
+ 
+
+
